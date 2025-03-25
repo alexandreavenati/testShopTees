6d3250a8e4f51fd9a9bd4e49523617f03f9991f0
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-03-25 15:32:24 +0100

    Shop tee-shirt avec message lors de la confirmation et stockage des valeurs envoyées en POST

--- view/create-order-view.php
+++ view/create-order-view.php
@@ -5,6 +5,10 @@
 
     <h2>Create a command</h2>
     <!-- Création d'un formulaire -->
+
+    <!-- Utilisation de la variable $message du controller de ce fichier -->
+    <h3><?php echo $message ?></h3>
+
     <form method="post">
 
         <!-- Champ de type texte pour mettre la quantité que l'on veut -->
@@ -15,16 +19,13 @@
         <!-- Champ de type selection pour dsélectionner un produit -->
         <label for="product">
             <select name="product" id="product">
-                <option value="tee-shirt Mario">Tee-shirt Mario</option>
-                <option value="tee-shirt Wario">Tee-shirt Wario</option>
+                <option value="tee-shirt_Mario">Tee-shirt Mario</option>
+                <option value="tee-shirt_Wario">Tee-shirt Wario</option>
             </select>
         </label>
 
         <button type="submit">Send Order</button>
     </form>
-
-    <!-- Utilisation de la variable $message du controller de ce fichier -->
-    <h3><?php echo $message ?></h3>
 
 </section>
 
