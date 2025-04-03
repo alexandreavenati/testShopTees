e0422c4c5f2ae4e3acefdc5e78ae369ed28720cf
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-04-03 14:43:36 +0200

    Projet Tees avec vérification sur la fonction createOrder

--- controller/create-order-controller.php
+++ controller/create-order-controller.php
@@ -1,4 +1,10 @@
 <?php
+// le contrôleur : 
+
+// récupère les données de requête (GET, POST etc etc)
+// appelle le(s) répository pour récupérer les données (bdd, session)
+// créé des variables / fonctions etc, pour simplifier l'utilisation des données dans la view
+// renvoie une réponse contenant le HTML généré par la view
 
 require_once('../config.php');
 // Appel du repository pour afficher les choix de produits dans la view
@@ -29,10 +35,3 @@ $OrderByUser = findOrderByUser();
 
 // Importation du fichier create-order-view (fichier avec le html)
 require_once('../view/create-order-view.php');
-
-// le controleur : 
-
-// récupère les données de requête (GET, POST etc etc)
-// appelle le(s) répository pour récupérer les données (bdd, session)
-// créé des variables / fonctions etc, pour simplifier l'utilisation des données dans la view
-// renvoie une réponse contenant le HTML généré par la view
