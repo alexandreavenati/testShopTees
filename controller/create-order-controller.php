369a2c54b54cede8b31cf3e750e156fe477d4db4
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-04-03 16:21:06 +0200

    Projet Tees avec l'utilisation d'Exception

--- controller/create-order-controller.php
+++ controller/create-order-controller.php
@@ -11,27 +11,28 @@ require_once('../config.php');
 require_once('../model/product-repository.php');
 require_once('../model/order-repository.php');
 
-$message = "";
+$message = "";  // Variable pour stocker les messages d'erreur
+
 //Je démarre la session : création d'un identifiant unique et l'associe à une zone de stockage sur le serveur,
 // qui l'envoie au navigateur (cookie)
 session_start();
 
-// condition qui vérifie l'envoi des valeurs dans le formulaire
-if (
-    array_key_exists("quantity", $_POST) &&
-    array_key_exists("product", $_POST)
-) {
-    $order = createOrder($_POST['product'], $_POST['quantity']);
-
-    //Si il y a une commande on la garde, sinon on envoie un msg d'erreur
+// Vérifie si le formulaire a été soumis
+if (array_key_exists("quantity", $_POST) && array_key_exists("product", $_POST)) {
     try {
+        // Tente de créer la commande
+        $order = createOrder($_POST['product'], $_POST['quantity']);
+        
+        // Si la commande est valide, on la sauvegarde dans la session
         saveOrder($order);
-    } catch(Exception $e) {
+    } catch (Exception $e) {
+        // Si une exception est levée, on capture l'erreur et on la stocke dans la variable $message
         $message = $e->getMessage();
     }
 }
 
+// Récupère la commande de l'utilisateur si elle existe dans la session
 $orderByUser = findOrderByUser();
 
-// Importation du fichier create-order-view (fichier avec le html)
+// Inclut la vue pour afficher le formulaire
 require_once('../view/create-order-view.php');
