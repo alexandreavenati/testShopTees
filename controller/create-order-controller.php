fe8b1f2fe5115b17925967d9a0cb30d24417f4aa
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-04-03 12:35:41 +0200

    Shop tees avec fonction pour les commandes

--- controller/create-order-controller.php
+++ controller/create-order-controller.php
@@ -23,3 +23,9 @@ $OrderByUser = findOrderByUser();
 // Importation du fichier create-order-view (fichier avec le html)
 require_once('../view/create-order-view.php');
 
+// le controleur : 
+
+// récupère les données de requête (GET, POST etc etc)
+// appelle le(s) répository pour récupérer les données (bdd, session)
+// créé des variables / fonctions etc, pour simplifier l'utilisation des données dans la view
+// renvoie une réponse contenant le HTML généré par la view
