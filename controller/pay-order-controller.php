3f85ee1e26fb7e76e5a7313e070cfa21203baa77
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-04-04 15:27:11 +0200

    Projet Tees avec changement de 'status'

--- controller/pay-order-controller.php
+++ controller/pay-order-controller.php
@@ -8,11 +8,10 @@ $orderByUser = findOrderByUser();
 
 // Vérifie si l'utilisateur a appuyé sur le bouton payer du form (method='post')
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-$orderByUser['status'] = "PAID";
-
-saveOrder($orderByUser);
-
+    // Changement du statut de la commande
+    $orderByUser['status'] = "PAID";
+    // Re-sauvegarde la commande avec le statut changé
+    saveOrder($orderByUser);
 }
 
 require_once('../view/pay-order-view.php');
