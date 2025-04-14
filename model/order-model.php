6acd0b8aea7f7b6ea83bfe7d708dc1a839bf7e3f
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-04-14 12:01:56 +0200

    Projet Tees création de class

---
+++ model/order-model.php
@@ -0,0 +1,16 @@
+<?php 
+// Je crée une classe
+class Order {
+
+    // je lui donne une variable "product"
+    public $product;
+
+    // je lui donne une variable "quantity"
+    public $quantity;
+
+    // je lui donne une variable "createdAt"
+    public $createdAt;
+
+    // je lui donne une variable "status"
+    public $status;
+}
