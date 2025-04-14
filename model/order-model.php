c7094c7d8147e415d02d2a1cf4487421262a26b5
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-04-14 12:04:23 +0200

    Projet Tees avec création de class

--- model/order-model.php
+++ model/order-model.php
@@ -2,15 +2,15 @@
 // Je crée une classe
 class Order {
 
-    // je lui donne une variable "product"
+    // je lui donne une variable "product" qui représentera le produit de la commande
     public $product;
 
-    // je lui donne une variable "quantity"
+    // je lui donne une variable "quantity" qui représentera la quantité de la commande
     public $quantity;
 
-    // je lui donne une variable "createdAt"
+    // je lui donne une variable "createdAt" qui représentera la date de création de la commande
     public $createdAt;
 
-    // je lui donne une variable "status"
+    // je lui donne une variable "status" qui représentera le statut de la commande
     public $status;
 }
